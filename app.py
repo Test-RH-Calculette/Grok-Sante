c610f056525d7f5f446bafc2ae85a2c45a5827fc
Test-RH-Calculette <rpenet@gmail.com>
2025-03-18 13:33:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -99,5 +99,5 @@ def dashboard():
 
 if __name__ == '__main__':
     init_db()
-    port = int(os.environ.get('PORT', 5000))  # Par défaut 5000 en local
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
